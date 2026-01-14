6b7a47e16a3367f96ed0b41bb144da878a675d21
othmanekahtal <contact@othmanekahtal.me>
2026-01-14 15:27:44 +0100

    implement login page and login logic in WebSecurityConfig

--- admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
+++ admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
@@ -17,29 +17,34 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-      .authorizeHttpRequests((requests)-> requests
-        .requestMatchers("/", "/index").permitAll()
-        .requestMatchers("/customers/**").hasRole("USER")
-        .requestMatchers("/orders").hasRole("ADMIN")
-        .anyRequest().authenticated()
-      )
-      .httpBasic(Customizer.withDefaults());
-      return http.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/index", "/login").permitAll()
+                        .requestMatchers("/customers/**").hasRole("USER")
+                        .requestMatchers("/orders").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login") // Point to our Controller mapping
+                        .permitAll()         // Allow everyone to see the login page
+                )
+                // 2. Configure Logout
+                .logout((logout) -> logout.permitAll());
+        return http.build();
+    }
 
-  @Bean
-  public GrantedAuthoritiesMapper authoritiesMapper(){
-    SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
-    authorityMapper.setConvertToUpperCase(true);
-    return authorityMapper;
-  }
+    @Bean
+    public GrantedAuthoritiesMapper authoritiesMapper() {
+        SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        return authorityMapper;
+    }
 
-  @Bean
-  public UserDetailsService userDetailsService(DataSource dataSource){
-    return new JdbcUserDetailsManager(dataSource);
-  }
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
 }
