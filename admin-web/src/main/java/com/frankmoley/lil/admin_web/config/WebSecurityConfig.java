ce8b9fbee81c78c35162608ced17c635b9d281f7
Othmane <othmanekahtal@yahoo.com>
2026-01-14 13:41:20 +0100

    my implementation

--- admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
+++ admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
@@ -1,20 +1,23 @@
 package com.frankmoley.lil.admin_web.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
-
+  @Autowired
+  private DataSource dataSource;
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
@@ -27,12 +30,7 @@ public class WebSecurityConfig {
   }
 
   @Bean
-  public UserDetailsService userDetailsService(){
-    UserDetails user = User.withDefaultPasswordEncoder()
-      .username("user")
-      .password("password")
-      .roles("USER")
-      .build();
-    return new InMemoryUserDetailsManager(user);
+  public UserDetailsService userDetailsService() {
+    return new JdbcUserDetailsManager(dataSource);
   }
 }
