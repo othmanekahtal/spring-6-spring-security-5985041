45711ba966db8c4e3622863b9f41343893f18610
othmanekahtal <contact@othmanekahtal.me>
2026-01-14 18:05:18 +0100

    implement auth with LDAP

--- admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
+++ admin-web/src/main/java/com/frankmoley/lil/admin_web/config/WebSecurityConfig.java
@@ -2,48 +2,64 @@ package com.frankmoley.lil.admin_web.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=landon,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
+    }
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-      .authorizeHttpRequests((requests)-> requests
-        .requestMatchers("/", "/index").permitAll()
-        .requestMatchers("/customers/**").hasRole("USER")
-        .requestMatchers("/orders").hasRole("ADMIN")
-        .anyRequest().authenticated()
-      )
-      .formLogin((form)-> form
-        .loginPage("/login")
-        .permitAll()
-      )
-      .logout((logout)-> logout.permitAll()
-      );
-      return http.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/index").permitAll()
+                        .requestMatchers("/customers/**").hasRole("USER")
+                        .requestMatchers("/orders").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll()
+                );
+        return http.build();
+    }
 
-  @Bean
-  public GrantedAuthoritiesMapper authoritiesMapper(){
-    SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
-    authorityMapper.setConvertToUpperCase(true);
-    return authorityMapper;
-  }
+    @Bean
+    public GrantedAuthoritiesMapper authoritiesMapper() {
+        SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        return authorityMapper;
+    }
 
-  @Bean
-  public UserDetailsService userDetailsService(DataSource dataSource){
-    return new JdbcUserDetailsManager(dataSource);
-  }
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
 }
